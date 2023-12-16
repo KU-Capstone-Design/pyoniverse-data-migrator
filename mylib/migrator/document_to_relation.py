2391f08d1538f0852efe1a14e85f7dd105a1bf80
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-12-16 15:21:45 +0900

    [feat] convert product info

--- mylib/migrator/document_to_relation.py
+++ mylib/migrator/document_to_relation.py
@@ -25,5 +25,26 @@ class DocumentToRelationMigrator(Migrator):
                         "good_count": int(s["good_count"]),
                         "view_count": int(s["view_count"]),
                     }
+            case "product_bests":
+                for s in src:
+                    yield {"product_id": int(s["id"]), "brand": int(s["best"]["brand"]), "price": s["best"]["price"]}
+            case "product_bests_product_events":
+                for s in src:
+                    for e in s["best"]["events"]:
+                        yield {"product_id": int(s["id"]), "event_id": int(e)}
+            case "product_brands":
+                for s in src:
+                    for b in s["brands"]:
+                        yield {
+                            "product_id": int(s["id"]),
+                            "brand_id": int(b["id"]),
+                            "price": int(b["price"]["value"]),
+                            "event_price": int(b["price"]["discounted_value"]),
+                        }
+            case "product_brands_product_events":
+                for s in src:
+                    for b in s["brands"]:
+                        for e in b["events"]:
+                            yield {"product_id": int(s["id"]), "brand_id": int(b["id"]), "event_id": int(e)}
             case _:
                 raise RuntimeError(f"{rel}은 지원하지 않습니다.")
--- tests/migrator/test_document_to_relation.py
+++ tests/migrator/test_document_to_relation.py
@@ -3,7 +3,7 @@ import os
 from mylib.migrator.document_to_relation import DocumentToRelationMigrator
 
 
-def test_convert_format(maria_driver, mongo_driver):
+def test_convert_format_product(maria_driver, mongo_driver):
     migrator = DocumentToRelationMigrator(src_driver=mongo_driver, dest_driver=maria_driver)
     src_data = mongo_driver.read(os.getenv("MONGO_DB"), "products", 100)
     dest_data = migrator._convert("products", src_data)
@@ -15,3 +15,43 @@ def test_convert_format(maria_driver, mongo_driver):
         assert d["image"] == s["image"]
         assert d["good_count"] == int(s["good_count"])
         assert d["view_count"] == int(s["view_count"])
+
+
+def test_convert_format_product_bests(maria_driver, mongo_driver):
+    migrator = DocumentToRelationMigrator(src_driver=mongo_driver, dest_driver=maria_driver)
+    src_data = mongo_driver.read(os.getenv("MONGO_DB"), "products", 100)
+    dest_data = migrator._convert("product_bests", src_data)
+    for s, d in zip(sorted(src_data, key=lambda x: x["id"]), sorted(dest_data, key=lambda x: x["product_id"])):
+        assert d["brand_id"] == int(s["best"]["brand"])
+        assert d["price"] == s["best"]["price"]
+
+
+def test_convert_format_product_bests_product_events(maria_driver, mongo_driver):
+    migrator = DocumentToRelationMigrator(src_driver=mongo_driver, dest_driver=maria_driver)
+    src_data = mongo_driver.read(os.getenv("MONGO_DB"), "products", 100)
+    dest_data = migrator._convert("product_bests_product_events", src_data)
+    for s, d in zip(sorted(src_data, key=lambda x: x["id"]), sorted(dest_data, key=lambda x: x["product_id"])):
+        for e in s["best"]["events"]:
+            assert d["event_id"] == int(e)
+
+
+def test_convert_format_product_brands(maria_driver, mongo_driver):
+    migrator = DocumentToRelationMigrator(src_driver=mongo_driver, dest_driver=maria_driver)
+    src_data = mongo_driver.read(os.getenv("MONGO_DB"), "products", 100)
+    dest_data = migrator._convert("product_brands", src_data)
+    for s, d in zip(sorted(src_data, key=lambda x: x["id"]), sorted(dest_data, key=lambda x: x["product_id"])):
+        for b in s["brands"]:
+            assert d["brand_id"] == int(b["id"])
+            assert d["price"] == b["price"]["value"]
+            assert d["event_price"] == b["price"]["discounted_value"]
+
+
+def test_convert_format_product_brands_product_events(maria_driver, mongo_driver):
+    migrator = DocumentToRelationMigrator(src_driver=mongo_driver, dest_driver=maria_driver)
+    src_data = mongo_driver.read(os.getenv("MONGO_DB"), "products", 100)
+    dest_data = migrator._convert("product_brands_product_events", src_data)
+    for s, d in zip(sorted(src_data, key=lambda x: x["id"]), sorted(dest_data, key=lambda x: x["product_id"])):
+        for b in s["brands"]:
+            assert d["brand_id"] == int(b["id"])
+            for e in b["events"]:
+                assert d["event_id"] == int(e)
